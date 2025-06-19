afe9a5f6a2970322c019b91fc6235e67e11a24ae
dinesh <dineshjustin95@gmail.com>
2025-06-20 02:28:11 +0530

    updated files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      jquery: resolve(__dirname, 'node_modules/jquery'),
+    },
+  },
+  optimizeDeps: {
+    include: ['jquery'],
+  },
+  build: {
+    sourcemap: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
